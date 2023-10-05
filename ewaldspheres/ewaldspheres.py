801dfc645a359cc133ff94068666d31cb0a4def6
Shervin Nia <niashervin@gmail.com>
2023-10-05 13:13:30 -0700

    added ewald animation

--- ewaldspheres/ewaldspheres.py
+++ ewaldspheres/ewaldspheres.py
@@ -1,4 +1,5 @@
 from manim import *
+from concurrent.futures import ThreadPoolExecutor
 
 class ewaldspheres(ThreeDScene):
     def construct(self):
@@ -32,3 +33,53 @@ class ThreeDParametricSpring(ThreeDScene):
         self.wait()
         self.play(Create(curve1))
         self.wait()
+
+
+
+count = 1
+def create_lattice_point(x, y, z):
+    global count
+    print(f"adding sphere {count}")
+    count = count+1
+    return Sphere(radius=0.01, color=BLUE).shift(x * 0.25* RIGHT + y * 0.25* UP + z * 0.25* OUT)
+
+class EwaldSphereRotation(ThreeDScene):
+    def construct(self):
+        # Initialize 3D view and axes
+        axes = ThreeDAxes()
+        self.set_camera_orientation(phi=75 * DEGREES, theta=45 * DEGREES)
+        self.add(axes)
+
+        # Initialize the reciprocal lattice
+        lattice_points = []
+
+        with ThreadPoolExecutor() as executor:
+            future_to_point = {(x, y, z): executor.submit(create_lattice_point, x, y, z)
+                               for x in range(-8, 12)
+                               for y in range(-8, 12)
+                               for z in range(-8, 12)}
+            
+            for future in future_to_point.values():
+                lattice_points.append(future.result())
+
+        self.add(*lattice_points)
+
+        
+        # Draw and label Ewald Sphere
+        ewald_sphere = Sphere(radius=1).set_fill(color = WHITE, opacity=0.5).move_to([1,0,0])  
+        #ewald_label = Text("Ewald Sphere").scale(0.2).next_to(ewald_sphere, direction=RIGHT, buff=0.5)
+        self.add(ewald_sphere)
+        
+        # Animate rotation of Ewald Sphere
+        self.play(Rotate(ewald_sphere, about_point=ORIGIN, angle=2*PI, run_time=30, rate_func=linear))
+
+        # TODO: Detect intersections with lattice points and draw reflection vectors
+        # You'll need custom logic to detect these intersections in real-time during the animation
+        # Once an intersection is detected, you can use the following code to draw a reflection vector
+        # intersection_point = ...  # Replace with actual coordinates
+        # reflection_vector = Arrow(ORIGIN, intersection_point, color=RED)
+        # reflection_label = Text("Reflection Vector").scale(0.2).next_to(reflection_vector, direction=RIGHT, buff=0.5)
+        # self.add(reflection_vector, reflection_label)
+        
+        # Optional: Zoom in/out
+        # self.play(self.camera.frame.animate.scale(0.5))
